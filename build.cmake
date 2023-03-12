2d6eda2cb482c5fd4faffd6656279267eb7b5abb
HominSu <hominsu@foxmail.com>
2023-03-12 10:49:44 +0800

    build: modfied cmake deps
    Signed-off-by: HominSu <hominsu@foxmail.com>

--- build.cmake
+++ build.cmake
@@ -1,18 +1,19 @@
 cmake_minimum_required(VERSION 2.8)
 
 set(build_dir ${CMAKE_CURRENT_LIST_DIR}/build)
+cmake_host_system_information(RESULT core_count QUERY NUMBER_OF_LOGICAL_CORES)
 
 if(NOT EXISTS ${build_dir})
     file(MAKE_DIRECTORY ${build_dir})
 endif()
 
 execute_process(
-        COMMAND ${CMAKE_COMMAND} ..
+        COMMAND ${CMAKE_COMMAND} -DNEUJSON_BUILD_EXAMPLES=ON -DNEUJSON_BUILD_TESTS=ON ..
         WORKING_DIRECTORY ${build_dir}
 )
 
 execute_process(
-        COMMAND ${CMAKE_COMMAND} --build .
+        COMMAND ${CMAKE_COMMAND} --build . --parallel ${core_count}
         WORKING_DIRECTORY ${build_dir}
 )
 
